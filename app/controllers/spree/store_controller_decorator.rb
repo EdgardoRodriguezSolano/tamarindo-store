4c211eb8ed369e18554e125a6408666c39ba265b
Edgardo Rodríguez <rodriguez_ed@hotmail.es>
2017-10-23 22:25:27 -0500

    Show featured products on homepage

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,8 +1,36 @@
 class Spree::StoreController
-  before_action :load_taxonomies
+  before_action :load_taxonomies, :load_featured
   
   def load_taxonomies
       @taxonomies = Spree::Taxonomy.includes(root: :children)
   end
   
+
+  def paginate(resource)
+    resource.
+      page(params[:page]).
+      per(params[:per_page] || default_per_page)
+  end
+
+  def default_per_page
+    500
+  end
+
+  def load_featured
+    @featured_taxon = Spree::Taxon.find("31") 
+    taxon = @featured_taxon
+    @featured_products = paginate(taxon.products.ransack(params[:q]).result)
+    @featured_products = @featured_products.includes(master: :default_price)
+   if params[:simple]
+          @exclude_data = {
+            variants: true,
+            option_types: true,
+            product_properties: true,
+            classifications: true
+          }
+          @product_attributes = %i(id name display_price)
+        end
+
+  end
+
 end

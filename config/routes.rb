206ceafe67c1f27b65007916179194d278694b2a
Edgardo Rodríguez <rodriguez_ed@hotmail.es>
2017-11-11 20:16:06 -0500

    Agregar botón 'update' en el cart form'

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   mount Spree::Core::Engine, at: '/'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
